c446c4c2c563b38242c569a34ea14169a9ed7fe0
Mchihiro0529 <glass7kurage@gmail.com>
2025-12-13 18:42:09 +0900

    add:Controllerのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,13 +8,16 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+     public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        // インスタンスの作成
-        $todo = new Todo();
-        $todos = $todo->all();
-        // dd($todos);
-
+        $todos = $this->todo->all();
         return view('todo.index', ['todos' => $todos]);
     }
 
@@ -29,26 +32,19 @@ class TodoController extends Controller
     //     dd('新規作成のルート実行！');
     // }
 
-    public function store(Request $request) // 追記
+    public function store(Request $request)
     {
-        $inputs = $request->all(); // 変更
-        // dd($inputs); // 追記
+        $inputs = $request->all();
 
-        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo(); 
-        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->fill($inputs);
-        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-        $todo->save();
-
+        $this->todo->fill($inputs);
+        $this->todo->save();
+        
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 
